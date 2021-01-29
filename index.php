9812cccc5600b0ff9521ebf4e403a1f9f8a59d7c
joannamfolk <jfolk2@mail.greenriver.edu>
2021-01-29 15:31:56 -0500

    added home view images style

--- index.php
+++ index.php
@@ -8,6 +8,14 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 $f3->set('DEBUG', 3);
 
+//Define a default route
+$f3->route('GET /', function() {
+
+    //Display a view
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
 $f3->route('GET /pets2', function () {
     $view = new Template();
     echo $view->render('views/home.html');
